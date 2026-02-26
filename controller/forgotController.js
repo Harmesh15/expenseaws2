ea30c9b62d0aaf01770c862c0e85b8424b1975f7
Harmesh15 <b180050@nitsikkim.ac.in>
2026-02-27 03:57:17 +0530

    mailchanges

--- controller/forgotController.js
+++ controller/forgotController.js
@@ -31,7 +31,7 @@ const sendmailTouser = async (req, res) => {
     });
 
     console.log(requestid, "reqid");
-    const resetLink = `${process.env.BASE_URL}/forgot/resetpassword.html?requestId=${requestid}`;
+    const resetLink = `/forgot/resetpassword.html?requestId=${requestid}`;
     await api.sendTransacEmail({
       sender: { email: "harmeshmahaur44@gmail.com", name: "ExpenseTracker" },
       to: [{ email }],
